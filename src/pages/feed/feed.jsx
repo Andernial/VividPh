8c47b99485130add5a8859231138503c76b9f365
Anderson <loonander@gmail.com>
2024-06-20 19:24:39 -0300

    feed router

--- src/pages/feed/feed.jsx
+++ src/pages/feed/feed.jsx
@@ -1,21 +1,39 @@
-import ProfilePic from "../../assets/images/example.jpeg"
 import { FaPlus } from "react-icons/fa";
 
 import { Cloudinary } from '@cloudinary/url-gen';
-import { useContext } from "react";
+import { useContext, useEffect, useState } from "react";
 import { ModalsContext } from "../../context/ModalsContext";
 import { AdvancedImage } from "@cloudinary/react";
 import CreateModal from "../../components/CreateModal";
 import { auto } from "@cloudinary/url-gen/actions/resize";
 import { autoGravity } from "@cloudinary/url-gen/qualifiers/gravity";
 import { YoutubePlayerContext } from "../../context/YoutublePlayerContext";
+import { FetchApi } from "../../utils/Fetch";
 
 function Feed() {
+    const [imagesData, setImagesData] = useState([])
+    const [requestLoading, setRequestLoading] = useState(false)
     const { postModalOpen, togglePostModal, navOpen } = useContext(ModalsContext)
     const { togglePlayer } = useContext(YoutubePlayerContext)
     const myProfile = true
     const cloudName = import.meta.env.VITE_CLOUD_NAME
+    const apiUrl = import.meta.env.VITE_API_URL
+    
+    const getImages = async () => {
+        setRequestLoading(true)
 
+        try {
+            const request = await FetchApi("GET", `${apiUrl}/post/show-all`,'')
+            setImagesData(request.results)
+            console.log(request)
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setRequestLoading(false)
+        }
+    }
+
+    // const myImage = cld.image('ud6cefpbtatpo0pbfret').resize(auto().gravity(autoGravity()).width(300).height(300));
     const cld = new Cloudinary({ // setando imagens com o cloudnary
         cloud: {
             cloudName: cloudName
@@ -23,12 +41,14 @@ function Feed() {
     })
 
 
-    const myImage = cld.image('ud6cefpbtatpo0pbfret').resize(auto().gravity(autoGravity()).width(200).height(200));
-
     const handlePostModal = () => {
         togglePlayer('')
         togglePostModal()
     }
+
+    useEffect(()=>{
+        getImages()
+    },[])
 
     return (
         <>
@@ -36,30 +56,26 @@ function Feed() {
                 <CreateModal />
             ) : null}
             <div className=" min-h-svh w-full bg-mainBg relative">
-                <div
-                    className=" w-full flex flex-col justify-center drop-shadow-lg bg-cover md:bg-center md:items-center relative"
-                    style={{ height: '50svh' }}
-                >
-                    {/* Overlay para escurecer a imagem de fundo */}
-                    <div className="absolute inset-0 bg-black opacity-50"></div>
-
-                    {/* Conteúdo sobre a imagem */}
-                    <div className="w-full flex flex-col justify-center items-center text-center text-white z-30 mt-10">
-                        <img src={ProfilePic} alt="Foto de Perfil" className={`size-24 rounded-full border-2 border-black`} />
-                        <p className="text-lg font-bold p-1">Sabbat</p>
-                        <p className=" w-60 pt-5">Olá eu gosto de tirar muitas fotos e esse é meu perfil !</p>
-                    </div>
-
-                </div>
-                <div className="w-full">
+    
+                <div className="w-full pt-12">
                     <h1 className="text-black font-bold text-center text-2xl py-7">
-                        {myProfile ? "Minhas fotos" : "Fotos Populares"}
+                        Fotos Recentes
                     </h1>
 
-                    <AdvancedImage cldImg={myImage} />
+                    <div className="w-full flex flex-row flex-wrap justify-center gap-5 p-5">
+                        {imagesData ? (
+                             imagesData.map((images,idx) => (
+
+                         <AdvancedImage  key={idx} cldImg={cld.image(`${images.image_public_id}`).resize(auto().gravity(autoGravity()).width(300).height(300))} className=" size-32" /> 
+                             ))
+                        ) : null}
+                    {/* <AdvancedImage cldImg={myImage} className=" size-32" /> */}
+                    </div>
+
+                   
 
                 </div>
-                {myProfile ? (
+          
                     <div className={`fixed w-full flex flex-col transition duration-200 scale-100 justify-end items-end z-20 top-3/4 ${postModalOpen ? 'invisible' : null}`}>
                         <button
                             className="flex justify-center items-center mr-5 size-16 rounded-full transition duration-500 ease-in-out bg-white border-2 border-black hover:bg-slate-300"
@@ -70,9 +86,7 @@ function Feed() {
                             <FaPlus />
                         </button>
                     </div>
-                ) : null}
-
-
+               
             </div>
         </>
     )
--- src/routes/router.jsx
+++ src/routes/router.jsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import Home from '../pages/home/home';
 import Profile from '../pages/profile/profile';
+import Feed from '../pages/feed/feed';
 
 const IndexRouter = () => {
 	return (
 		<Routes>
 			<Route path="/" element={<Home />} />
             <Route path="/Profile" element={<Profile />} />
+			<Route path="/Feed" element={<Feed />} />
 		</Routes>
 	);
 };
