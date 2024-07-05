430e616fe0fc34564c10d7c514c499b732b7f4a5
Anderson <loonander@gmail.com>
2024-07-05 18:17:21 -0300

    change bio and profile pic feature added

--- src/pages/profile/profile.jsx
+++ src/pages/profile/profile.jsx
@@ -1,10 +1,10 @@
-import ProfilePic from "../../assets/images/example.jpeg"
+import GenericProfilePic from "../../assets/images/example.jpeg"
 import { FaPlus } from "react-icons/fa";
 import { FaPencilAlt } from "react-icons/fa";
 import { IoMdClose } from "react-icons/io";
 
 import { Cloudinary } from '@cloudinary/url-gen';
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect, useRef, useState } from "react";
 import { ModalsContext } from "../../context/ModalsContext";
 import { AdvancedImage } from "@cloudinary/react";
 import CreateModal from "../../components/CreateModal";
@@ -20,10 +20,17 @@ import LoadingPosts from "../../assets/images/loading.svg"
 function Profile() {
     const { postModalOpen, togglePostModal, navOpen, viewPostModal, toggleViewPostModal, selectPostData, myPosts, setMyPosts } = useContext(ModalsContext)
     const { togglePlayer } = useContext(YoutubePlayerContext)
+    const [displayFile, setDisplayFile] = useState(null)
+    const [formItem, setFormItem] = useState(null)
+    const [atualProfilePic, setAtualProfilePic] = useState('generic')
     const [requestLoading, setRequestLoading] = useState(false)
+    const [editingPhoto, setEditingPhoto] = useState(false)
+    const [myInfo, setMyInfo] = useState(null)
     const [editingBio, setEditingBio] = useState(false)
 
     const myProfile = true
+
+    const bioTextRef = useRef(null);
     const cloudName = import.meta.env.VITE_CLOUD_NAME
     const { authUser } = useAuth()
     const apiUrl = import.meta.env.VITE_API_URL
@@ -49,9 +56,104 @@ function Profile() {
     }
 
 
+    const getProfileInfo = async () => {
+        setRequestLoading(true)
+
+        try {
+            const request = await FetchApi("GET", `${apiUrl}/user/show-by/${authUser.name}`, '')
+            setMyInfo(request.results)
+            console.log(request)
+            if (request.results && request.results.profilePic) {
+                console.log('caiu aqui')
+                const item = request.results.profilePic
+                return setAtualProfilePic(item.publicId)
+            }
+            setAtualProfilePic('generic')
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setRequestLoading(false)
+        }
+    }
+
+    const saveNewBio = async () => {
+        const bioText = bioTextRef.current.value;
+
+        try {
+            const object = { description: bioText }
+            const request = await FetchApi("PATCH", `${apiUrl}/user/update`, object, authUser.token)
+            setMyInfo(request.results)
+            console.log(request)
+
+            handleBioEdit()
+        } catch (error) {
+            console.log(error)
+        } finally {
+            setRequestLoading(false)
+        }
+    }
+
+
+    const selectImage = (files) => {
+        let file = files[0]
+        if (!file) { return }
+
+        const formData = new FormData()
+        setDisplayFile(URL.createObjectURL(file))
+        formData.append("file", file)
+        formData.append("upload_preset", "hnznzf2v")
+
+        setFormItem(formData)
+
+    }
+
+
+    
+    const uploadImage = async () => {
+        if (!displayFile) { return setError('Nenhuma Imagem Selecionada') }
+        setRequestLoading(true)
+
+        try {
+            const request = await FetchApi("POST", `https://api.cloudinary.com/v1_1/${cloudName}/image/upload`, formItem)
+            uploadPost(request)
+            console.log(request)
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setRequestLoading(false)
+        }
+    }
+
+    const uploadPost = async (requestData) => {
+        const data = { imageId:requestData.public_id ,imageUrl:requestData.url }
+        setRequestLoading(true)
+
+        console.log(data)
+
+        try {
+
+            const request = await FetchApi("POST", `${apiUrl}/user/create-pic`, data, authUser.token)
+           
+            console.log(request)
+            setAtualProfilePic(data.imageId)
+            handleEditPhotoModal()
+        } catch (error) {
+            console.log(error)
+        }finally{
+            setRequestLoading(false)
+        }
+    }
+
+
+
     const handlePostModal = () => {
         togglePlayer('')
         togglePostModal()
+    }
+
+    const handleEditPhotoModal = () => {
+        setDisplayFile('')
+        setEditingPhoto(!editingPhoto)
     }
 
     const handleBioEdit = () => {
@@ -64,12 +166,37 @@ function Profile() {
     }
 
     useEffect(() => {
-        console.log('caiu aqui')
         getImages()
+        getProfileInfo()
     }, [])
 
     return (
         <>
+            { editingPhoto ? (<div className="fixed h-svh w-full flex flex-col items-center justify-center z-30" style={{ backgroundColor: "rgba(0, 0, 0, 0.355)" }}>
+                <div className="bg-white flex flex-col items-center font-extrabold text-black text-2xl shadow-sm h-96 w-full">
+                    <div className="flex justify-end w-full p-5">
+                        <IoMdClose className="size-9 cursor-pointer" onClick={() => handleEditPhotoModal()} />
+                    </div>
+
+                    {displayFile ? <img src={displayFile} className={`size-24 rounded-full border-2 border-black cursor-pointer`} /> : null}
+
+                    {atualProfilePic === 'generic' && !displayFile ? <img src={GenericProfilePic} alt="Foto de Perfil" className={`size-24 rounded-full border-2 border-black cursor-pointer`} />
+                        :
+                        null}
+
+                     {atualProfilePic != 'generic' && !displayFile ?  <AdvancedImage cldImg={cld.image(`${atualProfilePic}`).resize(auto().gravity(autoGravity()).width(300).height(300))} className={`size-24 rounded-full border-2 border-black cursor-pointer`} /> : null}
+
+                   
+
+
+
+                    <input type="file" accept="image/*" className="text-sm" onChange={(e) => { selectImage(e.target.files) }} />
+                    <button className="p-1 bg-slate-300 border-1 border-black hover:bg-slate-400 disabled:opacity-75 disabled:hover:bg-slate-300 text-lg m-11 cursor-pointer" type="button" onClick={()=> uploadImage()} disabled={ !formItem || requestLoading}>Enviar</button>
+                </div>
+            </div>) : null}
+            
+
+
             {viewPostModal ? (
                 <PostModal />
             ) : null}
@@ -86,8 +213,11 @@ function Profile() {
 
                     {/* Conteúdo sobre a imagem */}
                     <div className="w-full flex flex-col justify-center items-center text-center text-white z-30 mt-10">
-    
-                        <img src={ProfilePic} alt="Foto de Perfil" className={`size-24 rounded-full border-2 border-black cursor-pointer`} />
+
+                        {atualProfilePic != 'generic' ? <AdvancedImage cldImg={cld.image(`${atualProfilePic}`).resize(auto().gravity(autoGravity()).width(300).height(300))} className={`size-24 rounded-full border-2 border-black cursor-pointer`} onClick={() =>handleEditPhotoModal()} /> 
+                            :
+                            <img src={GenericProfilePic} alt="Foto de Perfil" className={`size-24 rounded-full border-2 border-black cursor-pointer`} onClick={() =>handleEditPhotoModal()} />}
+
 
                         <p className="text-lg font-bold p-1">{authUser.name}</p>
                         <div className="flex items-center justify-center">
@@ -95,18 +225,21 @@ function Profile() {
                             {editingBio ?
                                 <>
                                     <div className="flex flex-col">
-                                        <textarea className="w-full h-20 text-black p-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
+                                        <textarea ref={bioTextRef} className="w-full h-20 text-black p-2 border border-gray-300 rounded-md resize-none focus:outline-none focus:ring-2 focus:ring-blue-500"></textarea>
                                         <div className="flex justify-between items-center">
-                                        <IoMdClose className=" size-10 cursor-pointer m-5" onClick={handleBioEdit} />
-                                        <FaPencilAlt className="size-7 cursor-pointer m-5" onClick={handleBioEdit} />
+                                            <IoMdClose className=" size-10 cursor-pointer m-5" onClick={handleBioEdit} />
+                                            <FaPencilAlt className="size-7 cursor-pointer m-5" onClick={() => saveNewBio()} />
                                         </div>
-                                        
+
                                     </div>
 
                                 </>
                                 : <>
-                                    <p className=" w-60 pt-5">Olá eu gosto de tirar muitas fotos e esse é meu perfil !</p>
-                                    <FaPencilAlt className="size-7 cursor-pointer" onClick={handleBioEdit} />
+                                    <div className="flex flex-col items-center">
+                                        <p className=" w-60 pt-5">{myInfo ? myInfo.description : null}</p>
+                                        <FaPencilAlt className="size-7 cursor-pointer" onClick={handleBioEdit} />
+                                    </div>
+
                                 </>}
 
                         </div>
@@ -121,7 +254,7 @@ function Profile() {
 
                     <div className="w-full flex flex-row flex-wrap justify-center gap-5 p-5 md:z-30">
 
-                                {requestLoading ? <img src={LoadingPosts} alt="loading" className="size-10" /> : null}
+                        {requestLoading ? <img src={LoadingPosts} alt="loading" className="size-10" /> : null}
 
                         {myPosts ? (
                             myPosts.map((images, idx) => (
